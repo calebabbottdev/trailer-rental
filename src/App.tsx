3598c202da1b1b06e520e61f6b729fb423240fe7
Caleb Abbott <cabbottdev@gmail.com>
2024-05-20 09:23:47 -0400

    Initial Firebase setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import app from './util/firebaseConfig';
 
 function App() {
-  console.log(import.meta.env.VITE_FIREBASE_API_KEY);
+  console.log('Vite: ', import.meta.env);
+  console.log('FB: ', app);
   const [count, setCount] = useState(0);
 
   return (
@@ -18,6 +20,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>{import.meta.env.VITE_FIREBASE_PROJECT_ID}</h2>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
